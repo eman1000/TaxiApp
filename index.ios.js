25b91595ba7d4d171214603d364d1f3310ed5d3b
eman1000 <emanzoelife@gmail.com>
2017-05-14 01:31:40 +0800

    Set up project for development

---
+++ index.ios.js
@@ -0,0 +1,45 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import Main from "./src/main";
+export default class TaxiApp extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Main/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('TaxiApp', () => TaxiApp);
---
+++ src/main.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+class Main extends React.Component {
+ render() {
+ 	return(
+ 		<View>
+ 			<Text>Hey Taxi App</Text>
+ 		</View>
+ 		);
+ 	
+ }
+
+
+}
+export default Main;
